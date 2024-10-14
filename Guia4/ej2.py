17a556760c61b83387c6f6680ae8ba9e19fea098
lucas-bircher <lucasbircher@gmail.com>
2024-10-14 19:22:29 -0300

    dd

--- Guia4/ej2.py
+++ Guia4/ej2.py
@@ -7,12 +7,17 @@ cant_max_it = 1000
 k = 3
 x = x[:,:-3]
 cant_columnas = cant_columnas-3
-print(x)
+
+# print(x)
+
+#### genera matriz de indices random para los k grupos
 indices = [[] for _ in range(k)]
 for i in range (cant_filas):
     n = random.randrange(k)
     indices[n].append(i)
 
+#### calcula la media de las entradas ubicadas en esos cluster
+#### para cada entrada
 medias = np.empty((k,cant_columnas),float)
 for j in range (k):
     suma = 0
@@ -23,26 +28,33 @@ for j in range (k):
 it=0
 while it<cant_max_it:
     it+=1
+    #### indices pasa a contener los indices con las distancias mínimas
     indices = [[] for _ in range(k)]
     medias_anterior = medias.copy()
     for i in range (cant_filas):
         dist_min = 10000
         indice_min = 0
         for j in range(k):
+            ##calcula distancia entre cada dato y el centroide del cluster definido anteriormente
+            ## como la media de los indices aleatorios
             vec_dist = x[i]-medias[j]
             dist = np.dot(vec_dist,vec_dist.T)
+            ## se guarda el indice del cluster con la distancia más chica
+            ## al centroide
             if(dist<dist_min):
                 indice_min = j
                 dist_min = dist
         indices[indice_min].append(i)
     for j in range (k):
         suma = 0
+        ## calculamos de nuevo el centroide con los indices actualizados
         for i in indices[j]:
             suma += x[i]
         if(len(indices[j])!=0):
             medias[j,:]= suma/len(indices[j])
     aux = medias - medias_anterior
 
+    ## si no hay diferencia entre las medias calculadas y las anteriores sale
     todos_ceros = True
     for fila in aux:
         for elemento in fila:
@@ -59,94 +71,88 @@ colores = ['r', 'g', 'b', 'c', 'm', 'y']
 
 #------ANCHO Y LONGITUD DEL SEPALO
 
-plt.figure(1)
-plt.title('Gráfico de clusters con sus medias respecto al ancho y a la longitud del sepalo')
-plt.xlabel('Eje X1')
-plt.ylabel('Eje X2')
-plt.grid(True)
+## defino subplot
+fig, axes = plt.subplots(3, 2, figsize=(12, 8))
+
+# Establecer las etiquetas de los ejes x e y
+for i in range(3):
+    for j in range(2):
+        axes[i, j].set_xlabel("Eje X1")
+        axes[i, j].set_ylabel("Eje X2")
+        # Habilitar la grilla
+        axes[i, j].grid(True)
+
+###############################
+axes[0, 0].set_title('Medias de clusters respecto al ancho y a la longitud del sepalo')
 for j in range(k):
     # Extraer los puntos correspondientes al cluster j
     puntos_cluster = x[indices[j]]
     
     # Graficar los puntos del cluster con un color distinto
-    plt.scatter(puntos_cluster[:, 0], puntos_cluster[:, 1], color=colores[j], label=f'Cluster {j+1}', marker='o')
+    axes[0,0].scatter(puntos_cluster[:, 0], puntos_cluster[:, 1], color=colores[j], label=f'Cluster {j+1}', marker='o')
 # Graficar las medias en color azul
-plt.scatter(medias[:, 0], medias[:, 1], color='black', marker='*', s=200, label='Centroides') 
+axes[0,0].scatter(medias[:, 0], medias[:, 1], color='black', marker='*', s=200, label='Centroides') 
 
 
-plt.figure(2)
-plt.title('Gráfico de clusters con sus medias respecto a la longitud del sepalo y la longitud del petalo')
-plt.xlabel('Eje X1')
-plt.ylabel('Eje X2')
-plt.grid(True)
+###############################
+axes[1, 0].set_title('Medias de clusters respecto a longitud del sepalo y del petalo')
 for j in range(k):
     # Extraer los puntos correspondientes al cluster j
     puntos_cluster = x[indices[j]]
     
     # Graficar los puntos del cluster con un color distinto
-    plt.scatter(puntos_cluster[:, 0], puntos_cluster[:, 2], color=colores[j], label=f'Cluster {j+1}', marker='o')
+    axes[1,0].scatter(puntos_cluster[:, 0], puntos_cluster[:, 2], color=colores[j], label=f'Cluster {j+1}', marker='o')
 # Graficar las medias en color azul
-plt.scatter(medias[:, 0], medias[:, 2], color='black', marker='*', s=200, label='Centroides') 
+axes[1,0].scatter(medias[:, 0], medias[:, 2], color='black', marker='*', s=200, label='Centroides') 
 
 
-plt.figure(3)
-plt.title('Gráfico de clusters con sus medias respecto a la longitud del sepalo y el ancho del petalo')
-plt.xlabel('Eje X1')
-plt.ylabel('Eje X2')
-plt.grid(True)
+###############################
+axes[2, 0].set_title('Medias de clusters respecto a la longitud del sepalo y el ancho del petalo')
 for j in range(k):
     # Extraer los puntos correspondientes al cluster j
     puntos_cluster = x[indices[j]]
     
     # Graficar los puntos del cluster con un color distinto
-    plt.scatter(puntos_cluster[:, 0], puntos_cluster[:, 3], color=colores[j], label=f'Cluster {j+1}', marker='o')
+    axes[2,0].scatter(puntos_cluster[:, 0], puntos_cluster[:, 3], color=colores[j], label=f'Cluster {j+1}', marker='o')
 # Graficar las medias en color azul
-plt.scatter(medias[:, 0], medias[:, 3], color='black', marker='*', s=200, label='Centroides') 
+axes[2,0].scatter(medias[:, 0], medias[:, 3], color='black', marker='*', s=200, label='Centroides') 
 
 
-plt.figure(4)
-plt.title('Gráfico de clusters con sus medias respecto al ancho del sepalo y la longitud del petalo')
-plt.xlabel('Eje X1')
-plt.ylabel('Eje X2')
-plt.grid(True)
+###############################
+axes[0, 1].set_title('Medias de clusters respecto al ancho del sepalo y la longitud del petalo')
 for j in range(k):
     # Extraer los puntos correspondientes al cluster j
     puntos_cluster = x[indices[j]]
     
     # Graficar los puntos del cluster con un color distinto
-    plt.scatter(puntos_cluster[:, 1], puntos_cluster[:, 2], color=colores[j], label=f'Cluster {j+1}', marker='o')
+    axes[0,1].scatter(puntos_cluster[:, 1], puntos_cluster[:, 2], color=colores[j], label=f'Cluster {j+1}', marker='o')
 # Graficar las medias en color azul
-plt.scatter(medias[:, 1], medias[:, 2], color='black', marker='*', s=200, label='Centroides') 
+axes[0,1].scatter(medias[:, 1], medias[:, 2], color='black', marker='*', s=200, label='Centroides') 
 
-
-plt.figure(5)
-plt.title('Gráfico de clusters con sus medias respecto al ancho del sepalo y el ancho del petalo')
-plt.xlabel('Eje X1')
-plt.ylabel('Eje X2')
-plt.grid(True)
+###############################
+axes[1,1].set_title('Medias de clusters respecto al ancho del sepalo y el ancho del petalo')
 for j in range(k):
     # Extraer los puntos correspondientes al cluster j
     puntos_cluster = x[indices[j]]
     
     # Graficar los puntos del cluster con un color distinto
-    plt.scatter(puntos_cluster[:, 1], puntos_cluster[:, 3], color=colores[j], label=f'Cluster {j+1}', marker='o')
+    axes[1, 1].scatter(puntos_cluster[:, 1], puntos_cluster[:, 3], color=colores[j], label=f'Cluster {j+1}', marker='o')
 # Graficar las medias en color azul
-plt.scatter(medias[:, 1], medias[:, 3], color='black', marker='*', s=200, label='Centroides') 
+axes[1, 1].scatter(medias[:, 1], medias[:, 3], color='black', marker='*', s=200, label='Centroides') 
 
 
-plt.figure(6)
-plt.title('Gráfico de clusters con sus medias')
-plt.xlabel('Eje X1')
-plt.ylabel('Eje X2')
-plt.grid(True)
+###############################
+axes[2,1].set_title('Gráfico de clusters con sus medias')
+
 for j in range(k):
     # Extraer los puntos correspondientes al cluster j
     puntos_cluster = x[indices[j]]
     
     # Graficar los puntos del cluster con un color distinto
-    plt.scatter(puntos_cluster[:, 2], puntos_cluster[:, 3], color=colores[j], label=f'Cluster {j+1}', marker='o')
+    axes[2,1].scatter(puntos_cluster[:, 2], puntos_cluster[:, 3], color=colores[j], label=f'Cluster {j+1}', marker='o')
 # Graficar las medias en color azul
-plt.scatter(medias[:, 2], medias[:, 3], color='black', marker='*', s=200, label='Centroides') 
+axes[2,1].scatter(medias[:, 2], medias[:, 3], color='black', marker='*', s=200, label='Centroides') 
 
 plt.legend()
+plt.tight_layout()
 plt.show()
--- Guía6/ej1.py
+++ Guía6/ej1.py
@@ -1,12 +1,54 @@
-#Inicializar población --> 1) crear individuos como cadenas de bits
+import numpy as np
+import random 
 
-#Evaluar-->1) traducir cadena de bit a parametros
-#       -->2) hacer funcion de fitness para evaluar
-#       -->3) guardar valores fitness
+cant_bits = 20
+
+x1 = -512
+x2 = 512
+cant_individuos = 15
+
+def gen_fen(x):
+    sum = 0
+    for i in range (len(x)):
+        sum += (2**(len(x)-i-1))*x[i]
+    
+    sum = x1 + sum/(x2-x1)
+    
+    return sum
+
+def fitness(x):
+    y = -x*np.sin(np.sqrt(np.abs(x)))
+    return -y
+
+
+#Inicializar población --> 1) crear individuos como cadenas de bits
+poblacion = np.empty((cant_individuos,cant_bits),int)
+
+for i in range(cant_individuos):
+    for j in range(cant_bits):
+        poblacion[i,j] = random.randint(0,1)
+
+#Evaluar-->1) traducir cadena de bit a parametros    
+vector_fen = np.empty(cant_individuos,float)
+for i in range(cant_individuos):
+    vector_fen[i] = gen_fen(poblacion[i,:])
+
+
+# -->2) evaluar y guardar valores fitness
+f = fitness(vector_fen)
+indices_ord = [i for i, _ in sorted(enumerate(vector_fen), key=lambda x: x[1])]
+
 
 #Repetir hasta cumplir aptitud
-    #Generar nueva poblacion
-        #seleccionar padres 
+cant_iteraciones = 20
+it = 0
+while(it < cant_iteraciones):
+ #Generar nueva poblacion
+     #seleccionar padres (metodo de ventana)
+    elite = poblacion[indices_ord[0]]
+    for i in range(cant_individuos): #cantidad de ventanas 
+        
+        
             #a) ruelta:  f = [4.5 2 3 0.5] divido sobre la suma
             #          =[0.45 0.2 0.3 0.05]
             # => hago sumo hasta cada uno: [0.45 0.6 0.95 1]
